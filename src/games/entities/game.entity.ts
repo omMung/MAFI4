877a791070f8a7d7b732c7c9830c983c5e51206b
YooDaeWon <dbeodnjs1001@naver.com>
2025-02-15 17:50:20 +0900

    Update game.entity.ts
    통계 테스트 위해 임시 구성

--- src/games/entities/game.entity.ts
+++ src/games/entities/game.entity.ts
@@ -1,1 +1,38 @@
-export class Game {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+export enum Role {
+  MAFIA = 'mafia',
+  POLICE = 'police',
+  DOCTOR = 'doctor',
+  CITIZEN = 'citizen',
+}
+
+@Entity({ name: 'Games' })
+export class Game {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column('boolean', { nullable: false })
+  isWin: boolean;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+  })
+  role: Role;
+
+  @ManyToOne(() => User, (user) => user.games, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+}
