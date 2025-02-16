4a8633af860e8c6bdbe1d0400cc2b87a583f11a9
SmoothMonologue <mobugy99@gmail.com>
2025-02-16 17:08:57 +0900

    add Like 엔티티 추가

--- src/likes/entities/like.entity.ts
+++ src/likes/entities/like.entity.ts
@@ -1,1 +1,16 @@
-export class Like {}
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Like {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+  //관계 설정
+  @ManyToOne(() => User, (user) => user.likes)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+  @ManyToOne(() => Post, (post) => post.likes)
+  @JoinColumn({ name: 'post_id' })
+  post: Post;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,42 +1,48 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    OneToMany,
-  } from 'typeorm';
-  import { Post } from '../../posts/entities/post.entity';
-  import { Comment } from '../../comments/entities/comment.entity';
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Post } from '../../posts/entities/post.entity';
+import { Comment } from '../../comments/entities/comment.entity';
 //   import { Ranking } from '../../ranking/ranking.entity';
-  import { UserAchievements } from '../../user-achievements/entities/users-achievement.entity';
-  @Entity()
-  export class User {
-    @PrimaryGeneratedColumn({ unsigned: true })
-    id: number;
-    @Column({ type: 'varchar', length: 255 })
-    email: string;
-    @Column({ type: 'varchar', length: 255 })
-    password: string;
-    @Column({ type: 'varchar', length: 255 })
-    nickName: string;
-    @Column({ type: 'boolean', default: false })
-    isVerified: boolean;
-    @Column({ type: 'varchar', length: 6 })
-    verifyCode: string;
-    @Column({ type: 'varchar', length: 255 })
-    title: string;
-    @CreateDateColumn({ type: 'timestamp' })
-    createdAt: Date;
-    @UpdateDateColumn({ type: 'timestamp' })
-    updatedAt: Date;
-    // 관계 설정
-    @OneToMany(() => Post, (post) => post.user)
-    posts: Post[];
-    @OneToMany(() => Comment, (comment) => comment.user)
-    comments: Comment[];
-    // @OneToMany(() => Ranking, (ranking) => ranking.user)
-    // rankings: Ranking[];
-    @OneToMany(() => UserAchievements, (userAchievements) => userAchievements.user)
-    userAchievements: UserAchievements[];
-  }
+import { UserAchievements } from '../../user-achievements/entities/users-achievement.entity';
+import { Like } from 'src/likes/entities/like.entity';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+  @Column({ type: 'varchar', length: 255 })
+  nickName: string;
+  @Column({ type: 'boolean', default: false })
+  isVerified: boolean;
+  @Column({ type: 'varchar', length: 6 })
+  verifyCode: string;
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+  // 관계 설정
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+  // @OneToMany(() => Ranking, (ranking) => ranking.user)
+  // rankings: Ranking[];
+  @OneToMany(
+    () => UserAchievements,
+    (userAchievements) => userAchievements.user,
+  )
+  userAchievements: UserAchievements[];
+  @OneToMany(() => Like, (like) => like.user)
+  likes: Like[];
+}
