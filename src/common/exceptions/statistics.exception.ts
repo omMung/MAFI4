f62af84b43360304cfcf7273c409465768bb70f0
YooDaeWon <dbeodnjs1001@naver.com>
2025-02-17 20:05:18 +0900

    Update statistics.exception.ts

--- src/common/exceptions/statistics.exception.ts
+++ src/common/exceptions/statistics.exception.ts
@@ -1,13 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 
-export class PostNotFoundException extends NotFoundException {
+export class userRecordNotFoundException extends NotFoundException {
   constructor() {
-    super('게시글를 찾을 수 없습니다.');
-  }
-}
-
-export class CommentNotFoundException extends NotFoundException {
-  constructor() {
-    super('댓글을 찾을 수 없습니다.');
+    super('게임 기록을 찾을 수 없습니다.');
   }
 }
