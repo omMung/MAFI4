60f833942609c61639a8c680a3f24fd2f16af214
didtjs123 <kjn90901@naver.com>
2025-02-17 10:00:16 +0900

    update auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from '../auth/services/auth.service';
 import { User } from '../users/entities/user.entity';
 import { AuthController } from '../auth/controllers/auth.controller';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { UsersService } from 'src/users/users.service';
-import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisModule } from 'src/redis/redis.module';
 import { AuthRepository } from './repositories/auth.repository';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), RedisModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    RedisModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '1h'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [UsersService, AuthService, AuthRepository, ConfigService],
+  providers: [AuthService, UsersService, AuthRepository],
   exports: [AuthService],
 })
 export class AuthModule {}
