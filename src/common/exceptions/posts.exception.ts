ea51a6d3675bc84b4f073fc89b156b22d2326ee3
thiswizard <dkaand3318@gmail.com>
2025-02-17 18:31:36 +0900

    fix posts exceptions 수정

--- src/common/exceptions/posts.exception.ts
+++ src/common/exceptions/posts.exception.ts
@@ -1,7 +1,5 @@
 import {
     NotFoundException,
-    BadRequestException,
-    ConflictException,
   } from '@nestjs/common';
   
   
@@ -10,31 +8,10 @@ import {
       super('게시글를 찾을 수 없습니다.');
     }
   }
-  
-  export class PostConflictException extends ConflictException {
+
+  export class CommentNotFoundException extends NotFoundException {
     constructor() {
-      super('이미 존재하는 게시글 입니다.');
+      super('댓글을 찾을 수 없습니다.');
     }
   }
-  
-  // // 댓글을 수정 또는 삭제할 권한이 없을 때 발생하는 예외
-  // export class CommentPermissionException extends NotFoundException {
-  //   constructor() {
-  //     super('댓글을 찾을 수 없거나 수정/삭제할 권한이 없습니다.');
-  //   }
-  // }
-  
-  // // 댓글 내용이 비었을 때 발생하는 예외
-  // export class EmptyCommentException extends BadRequestException {
-  //   constructor() {
-  //     super('유저 정보가 없습니다.');
-  //   }
-  // }
-  
-  // // 댓글 내용이 50자를 초과했을 때 발생하는 예외
-  // export class CommentLengthExceededException extends BadRequestException {
-  //   constructor() {
-  //     super('댓글 내용은 50자를 넘길 수 없습니다.');
-  //   }
-  // }
   
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -3,17 +3,13 @@ import {IsString , IsEnum , IsNotEmpty , IsNumber} from "class-validator"
 export class CreatePostDto {
 
 
-    @IsNumber()
-    @IsNotEmpty()
-    userId: number
-
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: '제목을 입력해주세요.' })
     title: string
 
 
     @IsString()
-    @IsNotEmpty()
+    @IsNotEmpty({ message: '내용을 입력해주세요.' })
     content: string
 
 }
