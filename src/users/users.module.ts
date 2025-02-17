9a4cf9be14621bcb586e809af5a083bdf8df94b8
chansikjeong <cjyong1004@naver.com>
2025-02-17 12:22:53 +0900

    update user,auth 프로바이더 수정
    둘의 레퍼지토리 추가, 유저쪽 레디스 모듈 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,13 @@ import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { User } from './entities/user.entity';
 import { AuthService } from 'src/auth/services/auth.service';
+import { RedisModule } from 'src/redis/redis.module';
+import { AuthRepository } from 'src/auth/repositories/auth.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), RedisModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, AuthService],
+  providers: [UsersService, UsersRepository, AuthService, AuthRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
