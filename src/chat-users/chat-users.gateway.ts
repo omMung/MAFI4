f9b4d57c4fb0cac48be8fe40ebe90cb7c10adfd5
thiswizard <dkaand3318@gmail.com>
2025-02-19 04:51:52 +0900

    fix 채팅방 코드 수정

--- src/chat-users/chat-users.gateway.ts
+++ src/chat-users/chat-users.gateway.ts
@@ -15,64 +15,75 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.connectedClients[client.id] = true;
   }
 
-  handleDisconnect(client: Socket): void {
-    delete this.connectedClients[client.id];
-    
-    Object.keys(this.roomUsers).forEach((room) => {
-      const index = this.roomUsers[room]?.indexOf(
-        this.clientNickname[client.id],
-      );
-      if (index !== -1) {
-        this.roomUsers[room].splice(index, 1);
-        this.server
-          .to(room)
-          .emit('userLeft', { userId: this.clientNickname[client.id], room });
-        this.server
-          .to(room)
-          .emit('userList', { room, userList: this.roomUsers[room] });
-      }
-    });
-
-  }
-
-
   @SubscribeMessage('setUserNick') // 유저가 원하는 닉네임 지정하기
   handleSetUserNick(client: Socket, nick: string): void {
     this.clientNickname[client.id] = nick;
   }
 
+  handleDisconnect(client: Socket): void {
+    delete this.connectedClients[client.id];
+
+    const existingRoom = Object.keys(this.roomUsers).find((roomName) =>
+      this.roomUsers[roomName].includes(this.clientNickname[client.id]))
+    
+    if(!existingRoom) {
+      return
+    }
+
+    const index = this.roomUsers[existingRoom]?.indexOf(
+      this.clientNickname[client.id],
+    );
+
+    if (index !== -1) {
+      this.roomUsers[existingRoom].splice(index, 1);
+      this.server
+        .to(existingRoom)
+        .emit('userLeft', { message: `${this.clientNickname[client.id]} 가 방에서 떠났습니다` });
+      this.server
+        .to(existingRoom)
+        .emit('userList', { message: `현재 방에 유저 목록은 ${this.roomUsers[existingRoom]}` });
+    }
+    
+  }
+
+
+  
+
 
   @SubscribeMessage('join') // 유저가 원하는 방에 진입하기
   handleJoin(client: Socket, room: string): void {
-    // 이미 접속한 방인지 확인합니다.
-    if (client.rooms.has(room)) {
+    
+    if (client.rooms.has(room)) { // 이미 접속한 방인지 확인합니다.
       return;
     }
 
-    if (!this.roomUsers[room]) {
-      this.roomUsers[room] = [];
+    const existingRoom = Object.keys(this.roomUsers).find((roomName) =>
+    this.roomUsers[roomName].includes(this.clientNickname[client.id]))
+
+    if (existingRoom) { // 이미 다른 방에 접속중인지 확인
+      client.emit('error', `이미 '${existingRoom}' 방에 참여 중입니다.`);
+      return;
     }
 
-    if (this.roomUsers[room].length >= 8) {
+    if (this.roomUsers[room].length >= 8) { // 방이 포화 상태인지 아닌지 확인
       client.emit('error','이미 방이 포화 상태 입니다')
       return
     }
-    client.join(room);
 
-    
+    if (!this.roomUsers[room]) { // 유저가 접속할려는 방이 없으면 방을 생성함
+      this.roomUsers[room] = [];
+    }
 
     this.roomUsers[room].push(this.clientNickname[client.id]);
+    
+    client.join(room);
+    
     this.server
       .to(room)
-      .emit('userJoined', { userId: this.clientNickname[client.id], room });
+      .emit('userJoined', {message: `방에 ${this.clientNickname[client.id]} 가 참여 했습니다` });
     this.server
       .to(room)
-      .emit('userList', { room, userList: this.roomUsers[room] });
-
-    this.server.emit('userList', {
-      room: null,
-      userList: Object.keys(this.connectedClients),
-    });
+      .emit('userList', {message: `현재 방에 유저 목록은 ${this.roomUsers[room]}` });
   }
 
 
@@ -84,55 +95,34 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       return; 
     }
 
-    client.leave(room);
-
     const index = this.roomUsers[room]?.indexOf(this.clientNickname[client.id]);
+
     if (index !== -1) {
       this.roomUsers[room].splice(index, 1);
+      client.leave(room)
       this.server
         .to(room)
-        .emit('userLeft', { userId: this.clientNickname[client.id], room });
+        .emit('userLeft', { message: `${this.clientNickname[client.id]} 가 방에서 떠났습니다` });
       this.server
         .to(room)
-        .emit('userList', { room, userList: this.roomUsers[room] });
+        .emit('userList', { message: `현재 방에 유저 목록은 ${this.roomUsers[room]}` });
     }
 
-    // 모든 방의 유저 목록을 업데이트하여 emit합니다.
-    Object.keys(this.roomUsers).forEach((room) => {
-      this.server
-        .to(room)
-        .emit('userList', { room, userList: this.roomUsers[room] });
-    });
-
-    // 연결된 클라이언트 목록을 업데이트하여 emit합니다.
-    this.server.emit('userList', {
-      room: null,
-      userList: Object.keys(this.connectedClients),
-    });
-  }
-
-
-  @SubscribeMessage('getUserList') // 방에 참여하는 사용자에게 유저 리스트를 전달
-  handleGetUserList(client: Socket, room: string): void {
-    this.server
-      .to(room)
-      .emit('userList', { room, userList: this.roomUsers[room] });
   }
 
 
   @SubscribeMessage('chatMessage') // 채팅 메시지를 각각의 방으로 전달
-  handleChatMessage(client: Socket , data: { message: string; room: string },): void
+  handleChatMessage(client: Socket , message: string ) : void
    {
     // 클라이언트가 보낸 채팅 메시지를 해당 방으로 전달합니다.
-    this.server.to(data.room).emit('chatMessage', {
-      userId: this.clientNickname[client.id],
-      message: data.message,
-      room: data.room,
-    });
+    const existingRoom = Object.keys(this.roomUsers).find((roomName) =>
+      this.roomUsers[roomName].includes(this.clientNickname[client.id]))
+
+    if (!existingRoom){
+      client.emit('error','방에 우선 참여를 해주세요')
+    }
+
+    this.server.to(existingRoom).emit('chatMessage', message )
+
   }
 }
-
-// socket.emit('chatMessage', { message: '안녕하세요!', room: 'room1' }); 클라이언트에서 메시지 전송
-//  socket.on('chatMessage', (data) => { // 클라이언트에서 메시지를 수신
-//   console.log(`[${data.room}] ${data.userId}: ${data.message}`);
-// });
