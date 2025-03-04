30d4b0bad609b2c054e06cb26efe13f8f13b6687
Hyeunggeun-Yun <gudrms121712@naver.com>
2025-03-04 10:09:45 +0900

    앤티티 추가 , 변경예정

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,12 @@
-export class Item {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Item {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'int' })
+  price: number;
+}
