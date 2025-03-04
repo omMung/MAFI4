da62ad0effe5df4a3b79d6795fdfb2ac0f6c58de
KimDongHwan <kdhn712@gmail.com>
2025-03-04 12:25:14 +0900

    update: 게임 저장 결과 로직

--- src/gameResults/gameResults.subscriber.ts
+++ src/gameResults/gameResults.subscriber.ts
@@ -1,16 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-// import { GameResult } from './entities/game-result.entity';
 import { GameResultsService } from './gameResults.service';
 import Redis from 'ioredis';
 
 @Injectable()
 export class GameResultsSubscriber implements OnModuleInit {
+  private redisSubscriber: Redis;
   private redisClient: Redis;
 
   constructor(private readonly gameResultsService: GameResultsService) {
+    // 일반 Redis 클라이언트 (데이터 조회 용)
     this.redisClient = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: 6379,
+    });
+
+    // 구독 전용 Redis 클라이언트
+    this.redisSubscriber = new Redis({
       host: process.env.REDIS_HOST || 'localhost',
       port: 6379,
     });
@@ -19,7 +24,7 @@ export class GameResultsSubscriber implements OnModuleInit {
   async onModuleInit() {
     console.log('게임 결과 Redis 구독 시작');
 
-    this.redisClient.subscribe('gameResults', (err, count) => {
+    this.redisSubscriber.subscribe('gameResults', (err, count) => {
       if (err) {
         console.error('Redis 구독 실패:', err);
       } else {
@@ -27,15 +32,16 @@ export class GameResultsSubscriber implements OnModuleInit {
       }
     });
 
-    this.redisClient.on('message', async (channel, message) => {
+    this.redisSubscriber.on('message', async (channel, message) => {
       if (channel === 'gameResults') {
         console.log(`게임 결과 수신: ${message}`);
         const gameResult = JSON.parse(message);
 
-        // Redis에서 다시 확인
+        // 일반 Redis 클라이언트를 사용하여 데이터 조회
         const storedResult = await this.redisClient.get(
           `gameResult:${gameResult.gameId}`,
         );
+
         if (!storedResult) {
           console.warn(
             `Redis에서 해당 게임 결과를 찾을 수 없음: ${gameResult.gameId}`,
