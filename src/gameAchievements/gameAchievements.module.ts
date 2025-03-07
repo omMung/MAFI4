a7a085eb329f8fe51f4195175a0be82600fb37a9
KimDongHwan <kdhn712@gmail.com>
2025-03-07 21:01:29 +0900

    update: 게임 업적 모듈 수정

--- src/gameAchievements/gameAchievements.module.ts
+++ src/gameAchievements/gameAchievements.module.ts
@@ -2,7 +2,6 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameAchievementsController } from './gameAchievements.controller';
 import { GameAchievementsService } from './gameAchievements.service';
-import { GameResultsSubscriber } from 'src/gameResults/gameResults.subscriber';
 import { GameAchievement } from './entities/gameAchievements.entity';
 import { GameResultsModule } from 'src/gameResults/gameResults.module';
 import { AchievementsModule } from 'src/achievements/achievements.module';
@@ -17,11 +16,7 @@ import { UserAchievementsModule } from 'src/user-achievements/users-achievements
     forwardRef(() => UserAchievementsModule),
   ],
   controllers: [GameAchievementsController],
-  providers: [
-    GameAchievementsService,
-    GameResultsSubscriber,
-    AchievementsService,
-  ],
+  providers: [GameAchievementsService, AchievementsService],
   exports: [GameAchievementsService],
 })
 export class GameAchievementsModule {}
