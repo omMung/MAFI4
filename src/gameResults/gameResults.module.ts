883488277b00a41d562ffb78046455b1785bebf3
KimDongHwan <kdhn712@gmail.com>
2025-03-10 20:50:36 +0900

    update: 유저 머니하고 점수 상승 하락 완료

--- src/gameResults/gameResults.module.ts
+++ src/gameResults/gameResults.module.ts
@@ -8,13 +8,16 @@ import { GameAchievementsModule } from 'src/gameAchievements/gameAchievements.mo
 import { AchievementsModule } from 'src/achievements/achievements.module';
 import { AchievementsService } from 'src/achievements/achievements.service';
 import { UserAchievementsModule } from 'src/user-achievements/users-achievements.module';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([GameResult]),
+    TypeOrmModule.forFeature([GameResult, User]),
     forwardRef(() => GameAchievementsModule),
     forwardRef(() => AchievementsModule),
     forwardRef(() => UserAchievementsModule),
+    forwardRef(() => UsersModule),
   ],
   controllers: [GameResultsController],
   providers: [GameResultsService, GameResultsSubscriber, AchievementsService],
